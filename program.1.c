184a7a05327264b9ead61922a13e3ccb8156c505
kr5623 <103554101+kr5623@users.noreply.github.com>
2022-04-22 10:33:45 +0530

    Create program.1.c

---
+++ program.1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+float BMI,KGs,CMs;
+printf("/nEnter the weight in KGs and height in CMs);
+scanf("%f%f%f",&BMI,&KGs,&CMs);
+BMI=weight/(height/100)**2;
+printf("BMI");
+return 0;
+}
+
